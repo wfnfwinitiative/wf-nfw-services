fbcfc36c1b8f061def4e05ea40329373a66a797c
rakesh <rakesh_anvekar@gmail.com>
2026-03-07 14:44:10 +0530

    inital code push

---
+++ app/schemas/opportunity_event_item_driver_schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import List
+
+
+from app.schemas.opportunity_event_schema import OpportunityEventCreate,OpportunityEventRead
+from app.schemas.opportunity_item_schemas import OpportunityItemRead, OpportunityItemCreate
+
+
+class OpportunityEventItemDriverRead(BaseModel):
+    event: OpportunityEventRead
+    items: List[OpportunityItemRead]
+
+    class Config:
+        from_attributes = True
+
+class OpportunityEventItemDriverCreate(BaseModel):
+    event_data: OpportunityEventCreate
+    items_data: List[OpportunityItemCreate]
